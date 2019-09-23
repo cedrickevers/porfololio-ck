5ea78fe13d946b22b7d4beff0863b40a1847a3e3
cedrickevers <keverscedric@gmail.com>
2019-09-23 23:02:34 +0200

    add skills mastery % -effect in js and styles in css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
 	$('#slides').superslides({
-		animation: "fade",
-		play: 5000
-	})
-
-	const typed = new Typed(".typed",{
-		strings: ["A RPG, turned based RPG.", " Anime, drama, series.", "Coding , SEO. "],
-		typeSpeed: 70,
-	    loop:true,
-	    startDelay:1000,
-	    showCursor: false
-
+		animation: 'fade',
+		play: 5000,
+		pagination: false
 	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4, 
+ 	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        700:{
+	            items:3
+	        },
+	        920:{
+	        	item:4
+	        }
+	    }
+	});
+	$('.chart').easyPieChart({
+		easing: " easeInOut",
+		barColor:"snow",
+		trackColor: false,
+		scaleColor:false,
+		lineWidth: 5,
+		size: 152,
+		onStep: function(from, to, percent) {
+			$(this.el).find('.percent')
+		}
+
+         });
 
 });
