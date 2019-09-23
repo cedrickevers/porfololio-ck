6b664a22bd68344b32d5ca21c85a1bbece7a23a7
cedrickevers <keverscedric@gmail.com>
2019-09-23 14:33:34 +0200

    Add writting effect on slide with typed lib

--- js/script.js
+++ js/script.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
 		play: 5000
 	})
 
+	const typed = new Typed(".typed",{
+		strings: ["A RPG, turned based RPG.", " Anime, drama , series.", "Coding , SEO. "]
+	});
+
 });
